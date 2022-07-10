35bacc5bc9f360d6bca5992fec8e6962579d98da
Kitty <82963444+KittyMcTophat@users.noreply.github.com>
2022-07-10 16:00:36 -0600

    Split Background into pixel and non-pixel verisons
    - Also added Long Yuri and a pixel version of Bee Yuri

--- TheScript.gd
+++ TheScript.gd
@@ -1,10 +1,12 @@
 extends ColorRect
 
+export var file_name : String = "DesktopBG";
+
 func _ready():
 	yield(get_tree(), "idle_frame");
 	yield(get_tree(), "idle_frame");
 	yield(get_tree(), "idle_frame");
 	var image_to_save = get_viewport().get_texture().get_data();
 	image_to_save.flip_y();
-	image_to_save.save_png("./DesktopBG.png");
+	image_to_save.save_png("./" + file_name + ".png");
 	get_tree().quit();
